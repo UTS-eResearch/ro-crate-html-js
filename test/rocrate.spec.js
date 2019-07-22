62fa6874814404a9b01b907a16d90eee24fe151c
Peter Sefton <peter.sefton@uts.edu.au>
2019-07-23 08:35:07 +1000

    Minimal start to the library - can create an empty RO-crate (without even having a @context yet)

---
+++ test/rocrate.spec.js
@@ -0,0 +1,35 @@
+/* This is part of rocrate-js a node library for implementing the RO-Crate data
+packaging spec. Copyright (C) 2019 University of Technology Sydney
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+const fs = require("fs");
+const path = require("path");
+const assert = require("assert");
+const ROCrate = require("../lib/rocrate");
+const jsonUtils = require("../lib/utils");
+
+describe("JSON-LD helper simple tests", function () {
+  var test_path;
+  const utils = new jsonUtils();
+  it("Test basic indexing", function (done) {
+    const crate = new ROCrate();
+    assert(utils.hasType(crate.rootDataset, "Dataset"));
+    done();
+  });
+});
+
+
+
---
+++ test/utils.spec.js
@@ -0,0 +1,36 @@
+/* This is part of rocrate-js a node library for implementing the RO-Crate data
+packaging spec. Copyright (C) 2019 University of Technology Sydney
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+const fs = require("fs");
+const path = require("path");
+const assert = require("assert");
+const jsonUtils = require("../lib/utils");
+
+describe("JSON-LD utilssimple tests", function () {
+  const utils = new jsonUtils();
+  it("Test basic indexing", function (done) {
+    const testItem = {"@type": "Person"}
+    assert(!utils.hasType(testItem, "Dataset"), "No false positive");
+    utils.addType(testItem, "Dataset")
+    assert(utils.hasType(testItem, "Dataset"), "Has type Dataset");
+    done();
+  });
+ 
+});
+
+
+
