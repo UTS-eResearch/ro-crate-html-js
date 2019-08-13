9b83719a3dc882058a4c24a98cda08a964b04a2b
Mike Lynch <mike@mikelynch.org>
2019-08-13 14:35:26 +1000

    Added addItem and addIdentifier to the ROCrate class

--- test/rocrate.spec.js
+++ test/rocrate.spec.js
@@ -18,11 +18,12 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 const fs = require("fs");
 const path = require("path");
 const assert = require("assert");
+const _ = require('lodash');
 const expect = require("chai").expect;
 const ROCrate = require("../lib/rocrate");
 const jsonUtils = require("../lib/utils");
 const defaults = require("../lib/defaults");
-
+const uuid = require('uuid').v4;
 
 function newCrate(graph) {
 	return new ROCrate({ '@context': defaults.context, '@graph': graph});
@@ -41,21 +42,94 @@ describe("JSON-LD helper simple tests", function () {
 });
 
 describe("Basic graph item operations", function() {
+	const graph = [
+  	 	{ '@id': 'https://foo/bar/oid1', 'name': 'oid1', 'description': 'Test item 1' },
+  		{ '@id': 'https://foo/bar/oid2', 'name': 'oid2', 'description': 'Test item 2' }
+	];
 
-	it("Can fetch items by id", function () {
-
-		const graph = [
-	  	 	{ '@id': 'https://foo/bar/oid1', 'name': 'oid1', 'description': 'Test item 1' },
-	  		{ '@id': 'https://foo/bar/oid2', 'name': 'oid2', 'description': 'Test item 2' }
-		];
-
-		const crate = newCrate(graph);
+	it("can fetch items by id", function () {
+		const crate = newCrate(_.clone(graph));
 		crate.index();
 
 		const item = crate.getItem('https://foo/bar/oid1');
 		expect(item).to.have.property('@id', 'https://foo/bar/oid1');
 
 	});
+
+	it("can add an item", function() {
+		const crate = newCrate(_.clone(graph));
+		crate.index();
+
+		const result = crate.addItem({
+			'@id': 'https://foo/bar/oid3', 'name': 'oid3', 'description': 'Test item 3'
+		});
+		expect(result).to.be.true;
+		const item = crate.getItem('https://foo/bar/oid3');
+		expect(item).to.have.property('@id', 'https://foo/bar/oid3');
+
+
+	});
+
+	it("can't add an item with an already existing id", function() {
+		const crate = newCrate(_.clone(graph));
+		crate.index();
+
+		const result = crate.addItem({
+			'@id': 'https://foo/bar/oid1', 'name': 'oid1', 'description': 'Duplicate ID'
+		});
+		expect(result).to.be.false;
+
+
+	});
+
+});
+
+describe("IDs and identifiers", function() {
+
+	it("can generate unique ids", function() {
+		const crate = newCrate();
+		crate.index();
+		const N = 20;
+
+		_.times(N, () => {
+			const id = crate.uniqueId('_:a');
+			const success = crate.addItem({'@id': id});
+			expect(success).to.be.true;
+		});
+
+		expect(crate.graph).to.have.lengthOf(N);
+	});
+
+
+	it("can add an identifier to the root dataset", function() {
+		const crate = newCrate([ { 
+			'@id': './',
+			'path': './',            // FIXME
+			'@type': 'Dataset',
+			'name': "Root dataset"
+		}]);
+
+		crate.index();
+
+		const myId = uuid();
+		const idId = crate.addIdentifier({
+			'identifier': myId
+		});
+
+		expect(idId).to.not.be.false;
+
+		const idItem = crate.getItem(idId);
+		expect(idItem).to.not.be.undefined;
+		expect(idItem).to.have.property("identifier", myId);
+
+		const rootDataset = crate.getRootDataset();
+		expect(rootDataset).to.have.property("identifier");
+		const rid = rootDataset['identifier'];
+		expect(rid).to.be.an('array').and.to.not.be.empty;
+		const match = rid.filter((i) => i['@id'] === idId);
+		expect(match).to.be.an('array').and.to.have.lengthOf(1);
+	});
+
 
 });
 
