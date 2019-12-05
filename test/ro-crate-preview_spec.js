1723df5c8542ee0f91a1e77d110eed8644ef6c03
Peter Sefton <peter.sefton@uts.edu.au>
2019-12-05 11:52:44 +1100

    Fixed test

--- test/ro-crate-preview_spec.js
+++ test/ro-crate-preview_spec.js
@@ -32,7 +32,7 @@ describe('single item rendering', function () {
       const crate = new ROCrate(json);
       const preview = new Preview(crate);
       const table = await preview.renderMetadataForItem(preview.rootId);
-      assert.equal(table.find("tr").length, 15, "Has the right number of rows");
+      assert.equal(table.find("tr").length, 16, "Has the right number of rows");
     });
 });
 
