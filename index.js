e601f3984bb00efff4c0b697178fa6f0967ee4de
Peter Sefton <peter.sefton@uts.edu.au>
2021-04-21 16:15:37 +1000

    Removed stray class from module

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 module.exports = {
     Preview: require("./lib/ro-crate-preview-wrapper.js"),
     HtmlFile: require("./lib/ro-crate-preview-file.js"),
-    Checker: require("./lib/checker.js"),
     Defaults: require("./lib/defaults.js"),
 };
