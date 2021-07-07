312610cb96247a48cfc63e2512d626f4b070ab42
Mike Lynch <Michael.Lynch@uts.edu.au>
2021-07-07 15:40:27 +1000

    Added very scanty geolocation stub test to check that my bugfix works

---
+++ test/geolocations_spec.js
@@ -0,0 +1,54 @@
+/* This is part of Calcyte a tool for implementing the DataCrate data packaging
+spec.  Copyright (C) 2018  University of Technology Sydney
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+const assert = require("assert");
+const fs = require("fs-extra");
+const Preview = require("../lib/ro-crate-preview-wrapper");
+const HtmlFile = require("../lib/ro-crate-preview-file");
+const { ROCrate } = require("ro-crate");
+const StaticPathUtils = require("../lib/static_utils");
+
+const chai = require("chai");
+chai.use(require("chai-fs"));
+
+describe("Stub geolocation tests", function () {
+    it("can run displayPlaces", async function () {
+        this.timeout(5000);
+        json = JSON.parse(
+            fs.readFileSync("test_data/sample-ro-crate-metadata.json")
+        );
+        const crate = new ROCrate(json);
+        const preview = new Preview(crate, {utils: new StaticPathUtils()});
+        const fc = {
+            type: "FeatureCollection",
+            features: [
+                {
+                type: "Feature",
+                geometry: {
+                    type: "Point",
+                    coordinates: [ 41.0000, 93.000 ]
+                    }
+                }   
+            ]
+        };
+        preview["places"] = fc;
+        const maphtml = await preview.displayPlaces();
+        assert(map, "Got a map");
+    });
+});
+
+after(function () {});
